e49798eaf33c5fe71ae6e9fad1b4141aa266f68c
Aakriti Aggarwal <56245613+aakriti1318@users.noreply.github.com>
2021-10-07 12:59:12 +0530

    Updated

--- Arrays/20 Moorie Voting_Majority Element.cpp
+++ Arrays/20 Moorie Voting_Majority Element.cpp
@@ -11,6 +11,30 @@ int main(){
     }
     
     // Moorie Voting Algo
-    
+
+    // Step 1: Finding the candidate element
+    int me = arr[0], count = 1;
+    for(int i=1;i<n;i++){
+        if(arr[i]!=arr[i-1]){
+            count--;
+            if(count == 0){
+                me = arr[i];
+                count++;
+            }
+        }
+    }
+    cout<<me;
+
+    // Step 2: Verify the candidate element as Majority Element
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        if(arr[i] == me){
+            cnt++;
+        }
+        if(cnt>=n/2){
+            cout<<"YES";
+            break;
+        }
+    }
     return 0;
 }
